9a2e9ac6713f470646e2d99075a2fcd4240fd5ca
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2022-03-24 18:34:18 +0700

    add scoring with TextMeshPro - Text
    there is no Text UI in my Unity, so the script is a little bit different

--- unity_exercise/Assets/Scripts/Managers/ScoreManager.cs
+++ unity_exercise/Assets/Scripts/Managers/ScoreManager.cs
@@ -1,18 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public static int score;
-
-
-    Text text;
-
+    TextMeshProUGUI text;
 
     void Awake ()
     {
-        text = GetComponent <Text> ();
+        text = GetComponent <TextMeshProUGUI> ();
         score = 0;
     }
 
